c7fdae5eb9cf55233b1bb864c7909b392dfba56f
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-03-28 13:46:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,25 +4,43 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
+const gravity = 0.5
+
 class Player {
-  constructor() {
-    this.position = {
+  constructor(position) {
+    this.position = position
+    this.velocity = {
       x: 0,
-      y: 0,
-    }
+      y: 1,
+  }
+    this.height = 100
   }
   
   draw() {
      c.fillStyle = 'red'
-  c.fillRect(this.position.x, this.position.y, 100, 100)
+  c.fillRect(this.position.x, this.position.y, 100, this.height)
   }
   
   update() {
-    this.position.y++
+    this.draw()
+    this.position.y += this.velocity.y
+    if (this.position.y + this.height + this.velocity.y < canvas.height) {
+    }
+    else {
+      this.velocity.y = 0
+    }
+      this.velocity.y += gravity
   }
 }
 
-const player = new Player()
+const player = new Player({
+  x: 0,
+  y: 0,
+})
+const player2 = new Player({
+  x: 300,
+  y: 100,
+})
 
 let y = 100
 let y2 = 100
@@ -31,14 +49,8 @@ function animate() {
   window.requestAnimationFrame(animate)
   c.fillStyle = 'white'
   c.fillRect(0, 0, canvas.width, canvas.height)
-  
-  c.fillStyle = 'red'
-  c.fillRect(200, y, 100, 100)
-  y++
-  
-  c.fillStyle = 'red'
-  c.fillRect(400, y2, 100, 100)
-  y2++
+  player.update()
+  player2.update()
 }
 
 animate()
