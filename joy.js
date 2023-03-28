0fed68a72886ff9ead528112893a93379d12541d
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-03-28 15:51:50 -0500

    Update joy.js

--- joy.js
+++ joy.js
@@ -1,1 +1,21 @@
-
+var joy = new JoyStick('joyDiv');
+var joy = new JoyStick('joyDiv',{
+    // The ID of canvas element
+    title: 'joystick',
+    // width/height
+    width: undefined,
+    height: undefined,
+    // Internal color of Stick
+    internalFillColor: 'black',
+    // Border width of Stick
+    internalLineWidth: 2,
+    // Border color of Stick
+    internalStrokeColor: 'white',
+    // External reference circonference width
+    externalLineWidth: 2,
+    //External reference circonference color
+    externalStrokeColor: 'white',
+    // Sets the behavior of the stick
+    autoReturnToCenter: true
+    
+});
