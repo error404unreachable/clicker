b69cf3e3b28888b6cac02c6296725c9d12be35b8
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-03-28 11:59:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
-
 let y = 100
 function animate() {
   window.requestAnimationFrame(animate)
+  c.fillStyle = 'white'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  
   c.fillStyle = 'red'
-  c.fillRect(200, 100, 100, 100)
+  c.fillRect(200, y, 100, 100)
   y++
 }
 
