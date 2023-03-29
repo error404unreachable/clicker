dc1c61a857f9df943ab1b56f9f730fa0d13c6c17
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-03-29 07:53:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -42,6 +42,15 @@ const player2 = new Player({
   y: 100,
 })
 
+const keys = {
+  d: {
+    pressed: false,},
+  a: {
+    pressed: false,},
+  w: {
+    pressed: false,},
+}
+
 let y = 100
 let y2 = 100
 
@@ -51,10 +60,35 @@ function animate() {
   c.fillRect(0, 0, canvas.width, canvas.height)
   player.update()
   player2.update()
+  
+  player.velocity.x = 0
+  if(keys.d.pressed) player.velocity.x = 5
+  else if (keys.a.pressed) player.velocity.x = -5
 }
 
 animate()
 
-window.addEventListener('keydown', () => {
-  console.log(event)
+window.addEventListener('keydown', (event) => {
+  switch (event.key){
+    case 'd':
+    keys.d.pressed = true
+    break
+    case 'a':
+    keys.a.pressed = true
+    break
+      case 'w':
+    player.velocity.y = -15
+    break
+  }
+})
+
+window.addEventListener('keyup', (event) => {
+  switch (event.key){
+    case 'd':
+    keys.d.pressed = false
+    break
+    case 'a':
+    keys.a.pressed = false
+    break
+  }
 })
