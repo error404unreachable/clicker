e63a400827ccdb92b63a57b638904ff4c1866d2d
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-04-11 12:08:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,19 +4,32 @@ const c = canvas.getContext('2d')
 canvas.width = 1500
 canvas.height = 1000
 
+class Player {
+   constructor() {
+      this.position = {
+          x: 0,
+          y: 0,
+      }
+   }
+    
+    draw() {
+        c.fillStyle = 'red'
+    c.fillRect(this.position.x, this.position.y, 100, 100)
+    } 
+    
+    update() {
+      this.position.y++  
+    }
+}
+
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-let y = 150
-let y2 = 150
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'white'
     c.fillRect(0, 0, canvas.width, canvas.height)
     
-    c.fillStyle = 'red'
-    c.fillRect(200, y, 100, 100)
-    y++
 }
 
 animate()
