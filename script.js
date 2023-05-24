02902487726f62e2ef60bf026031b5275f87a244
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-05-24 09:46:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let counter = 0;
 let onex = 1;
+let onexactive = 0;
+newInterval = setInterval(checkOther, 500)
 
   function addValue() {
     counter = counter + 1;
@@ -14,18 +16,21 @@ function welcomeUser() {
   alert("Welcome to Save the Earth... \n Start by tapping the Earth \n Upgrades will be automatic. \n Good luck!");
 }
   function checkUpgrades() {
-    if (counter == 10) {
+    if (counter == 10 && onexactive == 0) {
       myInterval = setInterval(autoClicker, 2000);
+      onexactive += 1;
     }
     else {
+      checkMore();
       console.log("Waiting for upgrades...");
     }
   }
   
 function checkMore() {
-  if (counter == 50) {
+  if (counter == 50 && onexactive == 1) {
     clearInterval(myInterval);
     onex += 1;
+    onexactive += 1;
     myInterval = setInterval(autoClicker, 1000);
   }
   else {
@@ -34,19 +39,20 @@ function checkMore() {
 }
 
   function checkAlerts() {
-    if (counter == 10) {
+    if (counter == 10 && onexactive == 0) {
       alert("You've unlocked something...");
       document.getElementById("log-input").innerText = "Rank increased to Rank 2 \n Achievement Unlocked! \n Energy Earner, Rank 1 (Earn 10 energy) \n \n";
       alert("You used your energy to restore an old power plant, and now it is producing you 1 energy per 2 seconds.");
       counter -= 10;
     }
     else {
+      checkOther();
       console.log("Waiting for upgrade...");
     }
   }
   
 function checkOther() {
-    if (counter == 50) {
+    if (counter == 50 && onexactive == 1) {
         alert("You've unlocked something...");
         document.getElementById("log-input").innerText = "Achievement Unlocked! \n Energy Earner, Rank 2 (Earn 50 energy) \n \n Achievement Unlocked! \n Upgrader, Rank 1 (Upgrade a power plant) \n \n";
         alert("You used your energy to upgrade the power plant. It now produces 2 energy per second.");
