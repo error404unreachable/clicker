fd1502481bf6a39c780a4e2268df707bb9515261
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-05-24 09:57:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let counter = 0;
 let onex = 1;
-let onexactive = 0;
 
   function addValue() {
     counter = counter + 1;
@@ -13,7 +12,7 @@ function welcomeUser() {
   alert("Welcome to Save the Earth... \n Start by tapping the Earth \n Upgrades will be automatic. \n Good luck!");
 }
   function checkUpgrades() {
-    if (counter == 10 && onexactive == 0) {
+    if (counter == 10) {
       myInterval = setInterval(autoClicker, 2000);
       onexactive += 1;
     }
@@ -23,7 +22,7 @@ function welcomeUser() {
   }
 
   function checkAlerts() {
-    if (counter == 10 && onexactive == 0) {
+    if (counter == 10) {
       alert("You've unlocked something...");
       document.getElementById("log-input").innerText = "Rank increased to Rank 2 \n Achievement Unlocked! \n Energy Earner, Rank 1 (Earn 10 energy) \n \n";
       alert("You used your energy to restore an old power plant, and now it is producing you 1 energy per 2 seconds.");
