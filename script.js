a43e9eee9fafd1e11ff627a1d565712a83be7a52
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-05-24 16:57:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,38 +13,40 @@ function welcomeUser() {
   alert("Welcome to Save the Earth... \n Start by tapping the Earth \n Upgrades will be automatic. \n Good luck!");
 }
 
-function tenEnergy() {
-  if (onexactive == 0) {
-    clearInterval(myInterval);
-    setInterval(autoClicker, 2000);
-    onexactive += 1;
-  }
-  else {
-    console.log("Already recieved!");
-  }
-}
-
-function fiftyEnergy() {
-  if (onexactive == 1) {
-    clearInterval(myInterval);
-    onex += 1;
-    myInterval = setInterval(autoClicker, 1000);
-  }
-  else {
-    console.log("You already have that!");
-  }
-}
-
   function checkUpgrades() {
     switch (counter) {
       case 10:
-        tenEnergy();
+        if (onexactive == 0) {
+    clearInterval(myInterval);
+    setInterval(autoClicker, 2000);
+    onexactive += 1;
+  };
+  else {
+    console.log("Already recieved!");
+  };
+        break;
       case 50:
-        fiftyEnergy();
+        if (onexactive == 1) {
+    clearInterval(myInterval);
+    onexactive += 1;
+    onex += 1
+    myInterval = setInterval(autoClicker, 1000);
+  };
+  else {
+    console.log("You already have that!");
+  };
+        break;
       case 150:
+        if (onexactive == 2) {
         clearInterval(myInterval);
         onex += 2;
+        onexactive += 1;
         myInterval = setInterval(autoClicker, 1000);
+        };
+        else {
+          console.log("You already have that!");
+        };
+        break;
       default:
         console.log("Waiting for upgrade...");
     }
