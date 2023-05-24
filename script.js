93ecdb633189cba8983626e0ca21751b68b03d8a
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-05-23 20:15:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let counter = 0;
   let onex = 0.5;
   document.getElementById('button').onclick = () => {
-    counter = + counter + 1;
+    counter = counter + 1;
     document.getElementById('score').innerText = counter;
     checkUpgrades();
     checkAlerts();
@@ -27,15 +27,14 @@ function welcomeUser() {
       let counter = counter -= 10;
     }
     else {
-      if (counter == 50) {
+      /*if (counter == 50) {
         alert("You've unlocked something...");
         document.getElementById("log-input").innerText = "Achievement Unlocked! \n Energy Earner, Rank 2 (Earn 50 energy) \n \n Achievement Unlocked! \n Upgrader, Rank 1 (Upgrade a power plant) \n \n";
         alert("You used your energy to upgrade the power plant. It now produces 2 energy per second.");
         let counter = counter -= 50;
         alert("Your friends and family support you in helping the planet, and fund you with 100 energy...");
         let counter = counter += 100;
-      }
-      else {
+      }*/
       console.log("Waiting for upgrade...");
       }
     }
