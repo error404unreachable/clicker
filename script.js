41c916007a3f5772864acf0cf2e268e21d4640d3
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-05-24 08:47:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function welcomeUser() {
 function checkMore() {
   if (counter == 50) {
     clearInterval(myInterval);
-    onex += 1.5;
+    onex += 1;
     myInterval = setInterval(autoClicker, 1000);
   }
   else {
@@ -40,18 +40,25 @@ function checkMore() {
       let counter = counter -= 10;
     }
     else {
-      /*if (counter == 50) {
+      checkOther();
+      console.log("Waiting for upgrade...");
+    }
+  }
+  
+function checkOther() {
+    if (counter == 50) {
         alert("You've unlocked something...");
         document.getElementById("log-input").innerText = "Achievement Unlocked! \n Energy Earner, Rank 2 (Earn 50 energy) \n \n Achievement Unlocked! \n Upgrader, Rank 1 (Upgrade a power plant) \n \n";
         alert("You used your energy to upgrade the power plant. It now produces 2 energy per second.");
         let counter = counter -= 50;
         alert("Your friends and family support you in helping the planet, and fund you with 100 energy...");
         let counter = counter += 100;
-      }*/
-      console.log("Waiting for upgrade...");
-    }
+      }
+  else {
+    console.log("Waiting for alerts...");
   }
-  
+}
+
   function autoClicker() {
     counter += onex;
     document.getElementById('score').innerText = counter;
