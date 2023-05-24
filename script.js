6bcac190e2ac2f22878321a9e6067513f5b09ac7
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-05-24 09:51:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 let counter = 0;
 let onex = 1;
 let onexactive = 0;
-newInterval = setInterval(checkOther, 500)
 
   function addValue() {
     counter = counter + 1;
     document.getElementById('score').innerText = counter;
     checkUpgrades();
     checkAlerts();
-    checkMore();
-    checkOther();
   }
   
 function welcomeUser() {
@@ -21,22 +18,9 @@ function welcomeUser() {
       onexactive += 1;
     }
     else {
-      checkMore();
       console.log("Waiting for upgrades...");
     }
   }
-  
-function checkMore() {
-  if (counter == 50 && onexactive == 1) {
-    clearInterval(myInterval);
-    onex += 1;
-    onexactive += 1;
-    myInterval = setInterval(autoClicker, 1000);
-  }
-  else {
-    console.log("Waiting for upgrades...");
-  }
-}
 
   function checkAlerts() {
     if (counter == 10 && onexactive == 0) {
@@ -46,24 +30,10 @@ function checkMore() {
       counter -= 10;
     }
     else {
-      checkOther();
       console.log("Waiting for upgrade...");
     }
   }
-  
-function checkOther() {
-    if (counter == 50 && onexactive == 1) {
-        alert("You've unlocked something...");
-        document.getElementById("log-input").innerText = "Achievement Unlocked! \n Energy Earner, Rank 2 (Earn 50 energy) \n \n Achievement Unlocked! \n Upgrader, Rank 1 (Upgrade a power plant) \n \n";
-        alert("You used your energy to upgrade the power plant. It now produces 2 energy per second.");
-        alert("Your friends and family support you in helping the planet, and fund you with 100 energy...");
-        counter += 50;
-      }
-  else {
-    console.log("Waiting for alerts...");
-  }
-}
-
+ 
   function autoClicker() {
     counter += onex;
     document.getElementById('score').innerText = counter;
