5fb589751e812e6b6b5b2416d5c380e343cf04b9
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-05-24 19:04:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -36,12 +36,12 @@ function welcomeUser() {
     console.log("You already have that!");
   };
         break;
-      case 150:
+      case 250:
         if (onexactive == 2) {
-        clearInterval(myInterval);
+        clearInterval(newInterval);
         onex += 2;
         onexactive += 1;
-        myInterval = setInterval(autoClicker, 1000);
+        newInterval = setInterval(autoClicker, 1000);
         };
         else {
           console.log("You already have that!");
@@ -55,13 +55,41 @@ function welcomeUser() {
   function checkAlerts() {
     switch (counter) {
       case 10:
+        if (onexactive == 0) {
         alert("You've unlocked something...");
         document.getElementById("log-input").innerText = "Rank increased to Rank 2 \n Achievement Unlocked! \n Energy Earner, Rank 1 (Earn 10 energy) \n \n";
         alert("You used your energy to restore an old power plant, and now it is producing you 1 energy per 2 seconds.");
+        }
+        else {
+          console.log("No alerts yet...");
+        }
+        break;
+      case 50:
+        if(onexactive == 1) {
+          alert("You've unlocked something...");
+          document.getElementById("log-input").innerText = "2 achievements unlocked! \n Energy Earner, Rank 2 (Earn 50 energy), \n Upgrader, Rank 1 (Upgrade any power plant) \n \n";
+          alert("You used your energy to upgrade the power plant.");
+          counter -= 50;
+          alert("Your friends and family support you helping the Earth. They fund you with 100 energy.");
+          counter += 100;
+        }
+        else {
+          console.log("No alerts yet...");
+        }
+        break;
+      case 250:
+        if (onexactive == 2) {
+          alert("You've unlocked something...");
+          document.getElementById("log-input").innerText = "Rank increased! \n Rank 2 --> Rank 3 \n Achievement Unlocked! \n Energy Earner, Rank 3 (Earn 250 energy) \n Achievement Unlocked! \n Restorer, Rank 1 (Restore 2 power plants) \n \n";
+          alert("You restored another power plant!");
+          alert("The new power plant produces you 4 energy per second!");
+        }
+        else {
+          console.log("Waiting for alerts...");
+        }
         break;
       default:
-      console.log("Waiting for upgrade...");
-        break;
+      console.log("Waiting for alerts...");
     }
   }
  
