d3d66b96b804273df0e8bd4fb512a006a1a87b23
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-05-24 19:17:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let counter = 0;
-let onex = 1;
-let onexactive = 0;
+var counter = 0;
+var onex = 1;
+var onexactive = 0;
 
   function addValue() {
     counter = counter + 1;
@@ -20,21 +20,21 @@ function welcomeUser() {
     clearInterval(myInterval);
     setInterval(autoClicker, 2000);
     onexactive += 1;
-  };
+  }
   else {
     console.log("Already recieved!");
-  };
+  }
         break;
       case 50:
         if (onexactive == 1) {
     clearInterval(myInterval);
     onexactive += 1;
-    onex += 1
+    onex += 1;
     myInterval = setInterval(autoClicker, 1000);
-  };
+  }
   else {
     console.log("You already have that!");
-  };
+  }
         break;
       case 250:
         if (onexactive == 2) {
@@ -42,10 +42,10 @@ function welcomeUser() {
         onex += 2;
         onexactive += 1;
         newInterval = setInterval(autoClicker, 1000);
-        };
+        }
         else {
           console.log("You already have that!");
-        };
+        }
         break;
       default:
         console.log("Waiting for upgrade...");
