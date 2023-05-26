cb07342fbf4489ed7db5c6627be6125aeb19216e
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-05-26 10:19:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let counter = 0;
 let onex = 1;
 let onexactive = 0;
 let twox = 4;
+let log = document.getElementById("log-input");
 
   function addValue() {
     counter = counter + 1;
@@ -64,33 +65,55 @@ function twofiftyUpgrades() {
     }
   }
 
-  function checkAlerts() {
-    switch (counter) {
-      case 10:
-        alert("You've unlocked something...");
-        document.getElementById("log-input").innerText = "Rank increased to Rank 2 \n Achievement Unlocked! \n Energy Earner, Rank 1 (Earn 10 energy) \n \n";
-        alert("You used your energy to restore an old power plant, and now it is producing you 1 energy per 2 seconds.");
-        setTimeout(2000);
-        counter -= 10;
-        alert("You found a hidden chest with energy inside!");
-        counter += 15;
-        break;
-      case 50:
-          alert("You've unlocked something...");
-          document.getElementById("log-input").innerText = "2 achievements unlocked! \n Energy Earner, Rank 2 (Earn 50 energy), \n Upgrader, Rank 1 (Upgrade any power plant) \n \n";
+function tenAlerts() {
+ if (onexactive == 0) {
+  alert("You've unlocked something...");
+   log.innerText = "Rank increased to Rank 2 \n Achievement Unlocked! \n Energy Earner, Rank 1 (Earn 10 energy) \n \n";
+   alert("You used your energy to restore an old power plant, and now it's producing you 1 energy per 2 seconds.");
+   counter -= 10;
+ }
+  else {
+    console.log("Waiting for alerts");
+  }
+}
+
+function fiftyAlerts() {
+  if (onexactive == 1) {
+    alert("You've unlocked something...");
+          log.innerText = "2 achievements unlocked! \n Energy Earner, Rank 2 (Earn 50 energy), \n Upgrader, Rank 1 (Upgrade any power plant) \n \n";
           alert("You used your energy to upgrade the power plant.");
           counter -= 50;
           alert("Your friends and family support you helping the Earth. They fund you with 100 energy.");
           counter += 100;
-        break;
-      case 250:
-          alert("You've unlocked something...");
-          document.getElementById("log-input").innerText = "Rank increased! \n Rank 2 --> Rank 3 \n Achievement Unlocked! \n Energy Earner, Rank 3 (Earn 250 energy) \n Achievement Unlocked! \n Restorer, Rank 1 (Restore 2 power plants) \n \n";
+  }
+  else {
+    console.log("Waiting for alerts...");
+  }
+}
+
+function twofiftyAlerts() {
+  if (onexactive == 2) {
+    alert("You've unlocked something...");
+          log.innerText = "Rank increased! \n Rank 2 --> Rank 3 \n Achievement Unlocked! \n Energy Earner, Rank 3 (Earn 250 energy) \n Achievement Unlocked! \n Restorer, Rank 1 (Restore 2 power plants) \n \n";
           alert("You restored another power plant!");
           counter -= 250;
           alert("The new power plant produces you 4 energy per second!");
-          counter += 50;
-          alert("The government has provided you with energy...");
+  }
+  else {
+    console.log("Waiting for alerts...");
+  }
+}
+
+  function checkAlerts() {
+    switch (counter) {
+      case 10:
+        tenAlerts();
+        break;
+      case 50:
+          fiftyAlerts();
+        break;
+      case 250:
+          twofiftyAlerts();
         break;
       default:
       console.log("Waiting for alerts...");
