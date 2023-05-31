86bf636b502f4be1a058727a330988873dd2dedf
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-05-31 18:47:05 -0500

    Create a.js

---
+++ a.js
@@ -0,0 +1,18 @@
+function welcomeUser() {
+  
+// Get the notification element
+		var notification = document.getElementById('notification');
+
+		// Set the text content of the notification
+		var message = "Welcome to Save the Earth. Start by tapping the Earth. Upgrades will be automatic. Good luck!";
+		notification.textContent = message;
+
+		// Show the notification
+		notification.style.display = 'block';
+
+		// Hide the notification after 5 seconds
+		setTimeout(function() {
+			notification.style.display = 'none';
+		}, 10000);
+  
+}
