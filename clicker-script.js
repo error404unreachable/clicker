e505c6854cb96a4814eb01772960b3bf5b3340ac
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2023-09-26 14:08:28 -0500

    Create clicker-script.js

---
+++ clicker-script.js
@@ -0,0 +1,10 @@
+let score = 0;
+
+function welcomeUser() {
+   alert("Welcome to Earth Clicker!");
+   alert("Start by tapping the Earth. \n Acheivements can be found in the log. \n Upgrades are automatic. \n \n Good Luck!");
+}
+
+function addScore() {
+   score = score + 1;
+}
